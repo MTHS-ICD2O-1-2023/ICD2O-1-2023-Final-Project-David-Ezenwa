12fecdbdab73d45b8ed191d434b2d2a578743737
David Ezenwa <159185092+david-ezenwa@users.noreply.github.com>
2024-06-08 19:33:25 +0000

    made a few tweeks

--- js/menuScene2.js
+++ js/menuScene2.js
@@ -7,43 +7,44 @@
 // This is the Menu Scene 2
 
 class MenuScene2 extends Phaser.Scene {
-    constructor() {
-        super({ key: "menuScene2" })
-
-        this.menuScene2BackgroundImage = null
-        this.replayButton = null
-        this.text = null
-        this.textStyle = { font: "65px Fira Sans", fill: "fff", align: "center" }
+    constructor () {
+      super({ key: 'menuScene2' })
+  
+      this.menuScene2BackgroundImage = null
+      this.replayButton = null
+      this.text = null
+      this.textStyle = { font: '65px Fira Sans', fill: '#fff', align: 'center' }
     }
-
-    init(data) {
-        this.cameras.main.setBackgroundColor("#050A30")
+  
+    init (data) {
+      this.cameras.main.setBackgroundColor('#050A30')
     }
-
-    preload() {
-        console.log("Menu Scene 2")
-
-        this.load.image("menuScene2_Background", "assets/Background.jpg")
-        this.load.image("replayButton", "assets/startButton.gif")
+  
+    preload () {
+      console.log('Menu Scene 2')
+  
+      this.load.image('menuScene2_galaxyBackground', 'assets/galaxyBackground.jpg')
+      this.load.image('replayButton', 'assets/startButton.gif')
     }
-    
-    create(data) {
-     this.menuScene2BackgroundImage = this.add.sprite(0, 0, "menuScene2_galaxyBackground")
-     this.menuScene2BackgroundImage.x = 1920 / 2
-     this.menuScene2BackgroundImage.y = 1080 / 2
-     
-     this.replayButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'replayButton')
-     this.replayButton.setInteractive({ useHandCursor: true })
-     this.replayButton.on('pointerdown', () => this.clickButton())
-
-     this.text = this.add.text(1100 / 2, (500 / 2) + 100, 'You finished the tutorial!\nPress start to go to the next level.', this.textStyle)
+  
+    create (data) {
+      this.menuScene2BackgroundImage = this.add.sprite(0, 0, 'menuScene2_galaxyBackground')
+      this.menuScene2BackgroundImage.x = 1920 / 2
+      this.menuScene2BackgroundImage.y = 1080 / 2
+  
+      this.replayButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'replayButton')
+      this.replayButton.setInteractive({ useHandCursor: true })
+      this.replayButton.on('pointerdown', () => this.clickButton())
+  
+      this.text = this.add.text(1100 / 2, (500 / 2) + 100, 'You finished the tutorial!\nPress start to go to the next level.', this.textStyle)
     }
-
-    update(time, delta) {
+  
+    update (time, delta) {
     }
-    clickButton() {
-        this.scene.start("gameScene2")
+  
+    clickButton () {
+      this.scene.start('gameScene2')
     }
-}
-
-export default MenuScene2
+  }
+  
+  export default MenuScene2
--- js/splashScene.js
+++ js/splashScene.js
@@ -4,35 +4,35 @@
 //
 // Created by: David.E
 // Created on: May 2024
-// This is the Splash Scene
+// This is the Splash Scene 
 
 class SplashScene extends Phaser.Scene {
-    constructor() {
-        super({ key: "splashScene" })
-
-        this.splashSceneBackgroundImage = null
+    constructor () {
+      super({ key: 'splashScene' })
+  
+      this.splashSceneBackgroundImage = null
     }
-
+  
     init (data) {
-        this.cameras.main.setBackgroundColor("#050A30")
+      this.cameras.main.setBackgroundColor('#050A30')
     }
-
-    preload() {
-        console.log("Splash Scene")
-        this.load.image("splashSceneBackground", "./assets/splashSceneBackground.png")
+  
+    preload () {
+      console.log('Splash Scene')
+      this.load.image('splashSceneBackground', './assets/splashSceneBackground.png')
     }
-
-    create(data) {
-        this.splashSceneBackgroundImage = this.add.sprite(0, 0, "splashSceneBackground")
-        this.splashSceneBackgroundImage.x = 1920 / 2
-        this.splashSceneBackgroundImage.y = 1080 / 2
+  
+    create (data) {
+      this.splashSceneBackgroundImage = this.add.sprite(0, 0, 'splashSceneBackground')
+      this.splashSceneBackgroundImage.x = 1920 / 2
+      this.splashSceneBackgroundImage.y = 1080 / 2
     }
-
-    update(time, delta) {
-        if(time > 5000) {
-            this.scene.switch("titleScene")
-        }
+  
+    update (time, delta) {
+      if (time > 5000) {
+        this.scene.switch('titleScene')
+      }
     }
-}
-
-export default SplashScene
+  }
+  
+  export default SplashScene
